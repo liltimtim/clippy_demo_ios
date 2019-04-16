c8e86fbd976871b901395dc33f7ac27255e11deb
Timothy Dillman <timothydillman@Timothys-MacBook-Pro.local>
2019-04-16 11:54:49 -0400

    init commit

---
+++ ClippDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClippDemo
+//
+//  Created by Timothy Dillman on 4/15/19.
+//  Copyright © 2019 Timothy Dillman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
